ea23f793317a831340f11c917316034fb2019a3c
belo4ya <41exey.kov41ev@gmail.com>
2023-02-18 21:09:58 +0300

    authelia configuration

--- services/streams/cmd/server/main.go
+++ services/streams/cmd/server/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+type RootResponse struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(RootResponse{Name: "Streams", Version: "1"})
+		if err != nil {
+			log.Printf("err: %s\n", err)
+		}
+	})
+	log.Fatal(http.ListenAndServe("localhost:8001", nil))
 }
